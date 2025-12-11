1c14914a43df3e08fbd3eb1703ea2f524c122c8c
RichardPickman <drezzerock@gmail.com>
2025-12-11 19:19:16 +0100

    utilize settings from constants and set them into html tag

--- src/content/scripts.ts
+++ src/content/scripts.ts
@@ -1,5 +1,20 @@
+import { settings } from '../constants';
+
+const html = document.querySelector('html');
+
+if (html) {
+    for (const key of settings) {
+        html.setAttribute(`disable_${key}`, 'false');
+    }
+}
+
 chrome.storage.onChanged.addListener((changes, areaName) => {
     if (areaName === 'local') {
         console.log('This is changes', changes);
+        for (const [key, value] of Object.entries(changes)) {
+            console.log(html);
+
+            html?.setAttribute(`disable_${key}`, String(value.newValue));
+        }
     }
 });
