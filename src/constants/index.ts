c37f371561475cc3481baf67c75a565e67b6ce71
RichardPickman <drezzerock@gmail.com>
2025-12-13 14:54:33 +0100

    move settings array to lib folder

--- src/constants/index.ts
+++
@@ -1,9 +0,0 @@
-export const settings = [
-    'shorts',
-    'recommends',
-    'description',
-    'comments',
-    'sidebar',
-    'ads',
-    'tags',
-];
---
+++ src/lib/constants/index.ts
@@ -0,0 +1,44 @@
+export type Setting =
+    | 'shorts'
+    | 'comments'
+    | 'sidebar'
+    | 'ads'
+    | 'tags'
+    | 'sponsor'
+    | 'description'
+    | 'description_merch'
+    | 'description_people_mentioned'
+    | 'description_teaser_carousel'
+    | 'description_video_summary'
+    | 'description_music'
+    | 'description_ticket'
+    | 'description_transcript'
+    | 'description_infocard'
+    | 'description_how_this_was_made'
+    | 'description_game_cards'
+    | 'description_chat_replay'
+    | 'description_ai_summary'
+    | 'related';
+
+export const settings: Setting[] = [
+    'shorts',
+    'comments',
+    'sidebar',
+    'ads',
+    'tags',
+    'sponsor',
+    'description',
+    'description_merch',
+    'description_people_mentioned',
+    'description_teaser_carousel',
+    'description_video_summary',
+    'description_music',
+    'description_ticket',
+    'description_transcript',
+    'description_infocard',
+    'description_how_this_was_made',
+    'description_game_cards',
+    'description_chat_replay',
+    'description_ai_summary',
+    'related',
+];
