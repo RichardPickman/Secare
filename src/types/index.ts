4a6037422642c54494cd35e6e42f6479daef8bce
RichardPickman <drezzerock@gmail.com>
2025-12-13 14:55:26 +0100

    remove types file and pick settings to constants

--- src/types/index.ts
+++
@@ -1,9 +0,0 @@
-export interface Settings {
-    shorts: boolean;
-    recommends: boolean;
-    description: boolean;
-    comments: boolean;
-    sidebar: boolean;
-    ads: boolean;
-    tags: boolean;
-}
