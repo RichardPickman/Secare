7c36fcf65dad38ecbb490642e37277b866a38dbe
RichardPickman <drezzerock@gmail.com>
2025-12-15 16:32:24 +0100

    add path aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
+import path from 'node:path';
 import { defineConfig } from 'vite';
 import webExtension from 'vite-plugin-web-extension';
 
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [webExtension(), react(), tailwindcss()],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+        },
+    },
 });
