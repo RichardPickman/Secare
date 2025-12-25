d289e92e912569b6bcb687ba7d5a0fb6d215ac53
RichardPickman <drezzerock@gmail.com>
2025-12-25 13:49:00 +0100

    move prepareSetting to utils

--- src/popup/utils/index.ts
+++ src/popup/utils/index.ts
@@ -2,3 +2,6 @@ import { clsx, type ClassValue } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
 export const cn = (...inputs: ClassValue[]) => twMerge(clsx(inputs));
+
+export const prepareSetting = (item: string) =>
+    item.includes('-') ? item.split('-').join(' ') : item;
