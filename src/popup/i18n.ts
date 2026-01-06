0f93bfe7e0f88a5c44527da660ea643e081766c5
RichardPickman <drezzerock@gmail.com>
2026-01-06 11:11:59 +0100

    add i18n config and utilize in main

---
+++ src/popup/i18n.ts
@@ -0,0 +1,18 @@
+import i18n from 'i18next';
+import HttpApi from 'i18next-http-backend';
+import { initReactI18next } from 'react-i18next';
+
+i18n.use(HttpApi)
+    .use(initReactI18next)
+    .init({
+        lng: 'en',
+        debug: true,
+        interpolation: {
+            escapeValue: false,
+        },
+        backend: {
+            loadPath: '/locales/{{lng}}/{{ns}}.json',
+        },
+    });
+
+export default i18n;
